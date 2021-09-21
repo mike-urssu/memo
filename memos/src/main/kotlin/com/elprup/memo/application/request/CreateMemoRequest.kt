5f06b9a3ebaf3be0e96006208c396b168296f643
Owner <morningstar9047@gmail.com>
2021-09-21 17:02:20 +0900

    feat: Memo 생성하기

---
+++ memos/src/main/kotlin/com/elprup/memo/application/request/CreateMemoRequest.kt
@@ -0,0 +1,18 @@
+package com.elprup.memo.application.request
+
+import com.elprup.memo.domain.model.entity.Memo
+import org.jetbrains.annotations.NotNull
+
+class CreateMemoRequest(
+    @field:NotNull
+    val title: String,
+    @field:NotNull
+    val content: String
+) {
+    fun toMemo(): Memo {
+        return Memo(
+            title = title,
+            content = content
+        )
+    }
+}
