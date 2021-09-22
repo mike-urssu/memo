82e521276c4936aea4ef159a0c9dadc7dbd8214b
Owner <morningstar9047@gmail.com>
2021-09-22 17:46:21 +0900

    .

--- memos/src/test/kotlin/com/elprup/memo/controller/GetMemoTest.kt
+++ memos/src/test/kotlin/com/elprup/memo/controller/GetMemoTest.kt
@@ -1,8 +1,6 @@
 package com.elprup.memo.controller
 
 import com.elprup.memo.common.MemoControllerTest
-import com.elprup.memo.domain.model.entity.Memo
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.test.web.servlet.get
