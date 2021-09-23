37c23ef6b569a6826e728c0d5b0be9238c284740
Owner <morningstar9047@gmail.com>
2021-09-24 00:14:32 +0900

    test: 특정 메모 조회 실패 테스트 코드

--- memos/src/test/kotlin/com/elprup/memo/service/GetMemoTest.kt
+++ memos/src/test/kotlin/com/elprup/memo/service/GetMemoTest.kt
@@ -1,16 +1,18 @@
 package com.elprup.memo.service
 
 import com.elprup.memo.common.MemoServiceTest
+import com.elprup.memo.domain.exception.MemoNotFoundException
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.mockito.BDDMockito.given
 import java.time.format.DateTimeFormatter
 import java.util.*
 
 class GetMemoTest : MemoServiceTest() {
     @Test
-    @DisplayName("특정 메모 조회하기")
+    @DisplayName("특정 메모 조회하기(성공)")
     fun getMemo_Success() {
         // given
         given(memoRepository.findById(memo.id!!))
@@ -24,5 +26,14 @@ class GetMemoTest : MemoServiceTest() {
         assertEquals(memo.title, getMemoDto.title)
         assertEquals(memo.content, getMemoDto.content)
         assertEquals(memo.updatedAt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")), getMemoDto.updatedAt)
+    }
+
+    @Test
+    @DisplayName("특정 메모 조회하기(실패-존재하지 않는 메모)")
+    fun getMemo_Fail_MemoNotFound() {
+        assertThrows<MemoNotFoundException> {
+            val invalidMemoId = Integer.MIN_VALUE
+            memoService.getMemo(invalidMemoId)
+        }
     }
 }
