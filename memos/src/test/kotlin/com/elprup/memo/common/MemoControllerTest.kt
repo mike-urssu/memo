49da1f9a72256a4b149e9f6bcb2ac27d3644829a
Owner <morningstar9047@gmail.com>
2021-09-23 18:17:30 +0900

    test: 메모 목록 조회 테스트 코드

--- memos/src/test/kotlin/com/elprup/memo/common/MemoControllerTest.kt
+++ memos/src/test/kotlin/com/elprup/memo/common/MemoControllerTest.kt
@@ -13,15 +13,31 @@ class MemoControllerTest : BaseControllerTest() {
 
     lateinit var memo: Memo
 
+    lateinit var memos: List<Memo>
+
     val invalidMemoId = -1
 
     @BeforeAll
     fun initData() {
         memo = createMockMemo()
+        memos = createMockMemos()
     }
 
     fun createMockMemo(): Memo {
         val memo = Memo(title = "test title", content = "test content")
         return memoRepository.save(memo)
+    }
+
+    fun createMockMemos(): List<Memo> {
+        val memos = mutableListOf<Memo>()
+        memos.add(Memo(title = "test title 0", content = "test content 0"))
+        memos.add(Memo(title = "test title 1", content = "test content 1"))
+        memos.add(Memo(title = "test title 2", content = "test content 2"))
+        memos.add(Memo(title = "test title 3", content = "test content 3"))
+        memos.add(Memo(title = "test title 4", content = "test content 4"))
+        memos.add(Memo(title = "test title 5", content = "test content 5"))
+        memos.add(Memo(title = "test title 6", content = "test content 6"))
+        memos.add(Memo(title = "test title 7", content = "test content 7"))
+        return memoRepository.saveAll(memos)
     }
 }
