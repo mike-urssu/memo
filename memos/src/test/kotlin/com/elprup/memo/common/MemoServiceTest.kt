dddf8f51667faa3bed85ca04238afdc403e2eacc
Owner <morningstar9047@gmail.com>
2021-09-24 23:53:39 +0900

    test: 메모 목록 조회하기 성공 서비스 테스트

--- memos/src/test/kotlin/com/elprup/memo/common/MemoServiceTest.kt
+++ memos/src/test/kotlin/com/elprup/memo/common/MemoServiceTest.kt
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 
 @ExtendWith(MockitoExtension::class)
 open class MemoServiceTest {
@@ -17,4 +19,6 @@ open class MemoServiceTest {
     lateinit var memoService: MemoService
 
     val memo = Memo(id = Integer.MAX_VALUE, title = "test title", content = "test content")
+
+    val defaultPageable = PageRequest.of(0, 5, Sort.by("updatedAt").descending())
 }
