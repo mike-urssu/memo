c8bf29c95af718c420d5ca7aa5f1e4e5184ded27
Owner <morningstar9047@gmail.com>
2021-09-24 18:54:24 +0900

    test: 메모 삭제하기 실패 서비스 테스트

--- memos/src/test/kotlin/com/elprup/memo/service/DeleteMemoTest.kt
+++ memos/src/test/kotlin/com/elprup/memo/service/DeleteMemoTest.kt
@@ -1,8 +1,10 @@
 package com.elprup.memo.service
 
 import com.elprup.memo.common.MemoServiceTest
+import com.elprup.memo.domain.exception.MemoNotFoundException
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito.verify
 import java.util.*
@@ -20,5 +22,14 @@ class DeleteMemoTest : MemoServiceTest() {
 
         //then
         verify(memoRepository).delete(memo)
+    }
+
+    @Test
+    @DisplayName("메모 삭제하기(실패-존재하지 않는 메모)")
+    fun deleteMemo_Fail_MemoNotFound() {
+        assertThrows<MemoNotFoundException> {
+            val invalidMemoId = Integer.MIN_VALUE
+            memoService.deleteMemo(invalidMemoId)
+        }
     }
 }
