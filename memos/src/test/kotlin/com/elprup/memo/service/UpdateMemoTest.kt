ae6bee2ac4d18cc2cd7e962501f09ce8c5f29b57
Owner <morningstar9047@gmail.com>
2021-09-24 18:14:24 +0900

    test: 메모 수정하기 실패 서비스 테스트

--- memos/src/test/kotlin/com/elprup/memo/service/UpdateMemoTest.kt
+++ memos/src/test/kotlin/com/elprup/memo/service/UpdateMemoTest.kt
@@ -2,9 +2,11 @@ package com.elprup.memo.service
 
 import com.elprup.memo.application.request.UpdateMemoRequest
 import com.elprup.memo.common.MemoServiceTest
+import com.elprup.memo.domain.exception.MemoNotFoundException
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.mockito.BDDMockito.given
 import java.util.*
 
@@ -25,5 +27,15 @@ class UpdateMemoTest : MemoServiceTest() {
         // then
         assertEquals(memo.title, updateMemoRequest.title)
         assertEquals(memo.content, updateMemoRequest.content)
+    }
+
+    @Test
+    @DisplayName("메모 수정하기(실패-존재하지 않는 메모)")
+    fun updateMemo_Fail_MemoNotFound() {
+        assertThrows<MemoNotFoundException> {
+            val invalidMemoId = Integer.MIN_VALUE
+            val updateMemoRequest = UpdateMemoRequest(title = "updated title", content = "updated content")
+            memoService.updateMemo(invalidMemoId, updateMemoRequest)
+        }
     }
 }
