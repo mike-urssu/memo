f7f9bade2869a673d2a222d12642daf6c87f0d1f
Joon Hee Song <mike.urssu@gmail.com>
2022-12-13 14:53:48 +0900

    Refactoring: 특정 메모 조회하기_성공 Test

--- memos/src/test/kotlin/com/elprup/memo/service/MemoServiceTest.kt
+++ memos/src/test/kotlin/com/elprup/memo/service/MemoServiceTest.kt
@@ -1,5 +1,6 @@
 package com.elprup.memo.service
 
+import com.elprup.memo.domain.model.dto.GetMemoDto
 import com.elprup.memo.domain.model.entity.Memo
 import com.elprup.memo.domain.model.repository.MemoRepository
 import com.elprup.memo.domain.service.MemoService
@@ -7,14 +8,14 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito.lenient
 import org.mockito.junit.jupiter.MockitoExtension
-import org.springframework.transaction.annotation.Transactional
+import java.util.Optional
 
 @ExtendWith(MockitoExtension::class)
-@Transactional
 class MemoServiceTest {
     @InjectMocks
     lateinit var memoService: MemoService
@@ -37,5 +38,27 @@ class MemoServiceTest {
 
         // then
         assertEquals(memo, createdMemo)
+    }
+
+    @Test
+    @DisplayName("특정 메모 조회하기_성공")
+    fun getMemo_Success() {
+        val id = 1
+        val title = "title for service unit test"
+        val content = "content for service unit test"
+        val memo = Memo(title, content)
+        memo.id = id
+        val expectedDto = GetMemoDto(memo)
+
+        // given
+        given(memoRepository.findById(id)).willReturn(Optional.of(memo))
+
+        //when
+        val actualDto = memoService.getMemo(id)
+
+        //then
+        assertEquals(expectedDto.memoId, actualDto.memoId)
+        assertEquals(expectedDto.title, actualDto.title)
+        assertEquals(expectedDto.content, actualDto.content)
     }
 }
